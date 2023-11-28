db25b3375d1258ede786fa952968eced57b109e6
Toby58U <tobennaumeh001@gmail.com>
2023-11-28 12:36:52 -0800

    If, else, loops, and functions commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for code in range(97, 123):  # ASCII codes for lowercase letters
-    if code != 101 and code != 113:  # Excluding 'e' (101) and 'q' (113)
-        print(chr(code), end='')  # Convert ASCII code to character and print without a new line
-
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
