5ffb88fff3ec13e4322fe5e4cbb89012447d544e
Toby58U <tobennaumeh001@gmail.com>
2023-12-03 13:11:20 -0800

    Python data structures commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	long int size, allocated, i;
+	PyListObject *list_obj = (PyListObject *)p;
+
+	size = PyList_Size(p);
+	allocated = list_object -> allocated;
+
+	printf("[*} Size of the Python list = %li\n", size);
+	printf("[*] Allocated slots = %li\n", allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %li: %s\n", i, Py_TYPE(list_obj -> ob_item[i] -> tp_name);
+	}
+}
---
+++ 0x03-python-data_structures/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    original_list = [1, 2, 3, 4, 5]
+    index_to_replace = 2
+    new_element = 10
+
+    result = new_in_list(original_list, index_to_replace, new_element)
+
+    print("Original List:", original_list)
+    print("Resulting List:", result)
