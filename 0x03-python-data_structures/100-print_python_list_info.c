08e1ac8de465afe762960ddeaaa7a263c2a2a72c
Toby58U <tobennaumeh001@gmail.com>
2023-12-03 13:13:22 -0800

    Python data structures commit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,13 +8,13 @@ void print_python_list_info(PyObject *p)
 	PyListObject *list_obj = (PyListObject *)p;
 
 	size = PyList_Size(p);
-	allocated = list_object -> allocated;
+	allocated = list_object->allocated;
 
 	printf("[*} Size of the Python list = %li\n", size);
 	printf("[*] Allocated slots = %li\n", allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Element %li: %s\n", i, Py_TYPE(list_obj -> ob_item[i] -> tp_name);
+		printf("Element %li: %s\n", i, Py_TYPE(list_obj->ob_item[i]->tp_name);
 	}
 }
