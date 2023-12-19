0e13047c82d85a9a509e776c720120342ac0957f
Toby58U <tobennaumeh001@gmail.com>
2023-12-19 07:10:14 -0800

    Python classes commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
 class Square:
+    """Represent a square."""
     def __init__(self, size=0):
         self.__size = size
         self.__validate_size()
     def __validate_size(self):
+
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         elif self.__size < 0:
