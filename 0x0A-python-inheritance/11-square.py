d8894e5c25c69b4cbea4096b214c03360d591f6f
Toby58U <tobennaumeh001@gmail.com>
2024-01-08 14:41:29 -0800

    Python inheritance commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -20,4 +20,5 @@ class Square(Rectangle):
         return self.__size**2
 
     def __str__(self):
-        return "[{}] {:d}/{:d}".format(self.__class__.__name__, self.__size, self.__size)
+        return "[{}] {:d}/{:d}".format(self.__class__.__name__,
+                                        self.__size, self.__size)
