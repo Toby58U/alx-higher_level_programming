f2b1f2c927e2eebccf1d21eccdbaebb40a312882
Toby58U <tobennaumeh001@gmail.com>
2024-01-08 14:44:01 -0800

    Python inheritance commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 """Module implementing Square class"""
-Rectangle = __import__("9-rectangle").Rectangle
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class to represent a Square as a subclass of Rectangle"""
+    """Represent a square."""
+
     def __init__(self, size):
-        """Initialize new Square object
+        """Initialize a new square.
 
         Args:
-            size (int): side lengths
+            size (int): The size of the new square.
         """
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
-
-    def area(self):
-        """Compute area of square"""
-        return self.__size**2
-
-    def __str__(self):
-        return "[{}] {:d}/{:d}".format(self.__class__.__name__,
-                                        self.__size, self.__size)
+        self.__size = size
