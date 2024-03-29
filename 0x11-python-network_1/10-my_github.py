562f461ca7275ffc8849c52ea75eec3463bb875a
Toby58U <tobennaumeh001@gmail.com>
2024-03-29 16:30:17 -0700

    Python network commit

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,3 +8,8 @@ import sys
 
 
 if __name__ == "__main__":
+    base_url = 'https://api.github.com/user'
+    username = sys.argv[1]
+    password = sys.argv[2]
+    res = get(base_url, auth=auth.HTTPBasicAuth(username, password))
+    print(res.json().get('id'))
