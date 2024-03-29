82db84e098eddca352ccaf392a9843d86becaf1a
Toby58U <tobennaumeh001@gmail.com>
2024-03-29 15:13:58 -0700

    Python network commit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+(handling HTTP errors)
+"""
+from urllib import request, error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('UTF-8'))
+    except error.HTTPError as e:
+        print('Error code:', e.code)
