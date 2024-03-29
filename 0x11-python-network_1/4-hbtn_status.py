7e59e9e3340b6afb5205a5cdad23b62b1c1c7680
Toby58U <tobennaumeh001@gmail.com>
2024-03-29 15:29:56 -0700

    Python network commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     url = https://intranet.hbtn.io/status
     response = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
